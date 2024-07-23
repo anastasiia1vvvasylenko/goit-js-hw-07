92d77454204f6f0b62dcdc0a0e2c9ea1af644f69
AnastasiiaV <166251420+anastasiia1vvvasylenko@users.noreply.github.com>
2024-07-23 21:54:21 +0200

    ready-hw

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,37 @@
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  }
+});
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  destroyBoxes();
+  let size = 30;
+  let boxesHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const color = getRandomHexColor();
+    boxesHTML += `<div style='width: ${size}px; height: ${size}px; background-color: ${color};'></div>`;
+    size += 10;
+  }
+  boxesContainer.insertAdjacentHTML('beforeend', boxesHTML);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
